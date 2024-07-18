fe304313f60927636a5970184997fe0ed9bede3f
9909hjh <9909hjh@gmail.com>
2024-07-19 01:06:19 +0900

    서버 기능 테스트

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
+        static void MainThread()
+        {
+            Console.WriteLine("Hello Thread");
+        }
+
         // 핵심 기능
         static void Main(string[] args)
         {
+            Thread t = new Thread(MainThread);
+            t.Start();
+
             Console.WriteLine("Hello, World!");
         }
     }
