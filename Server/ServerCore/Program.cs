121c7e21e9d81227e17cd5bb8794df9af0d82104
9909hjh <9909hjh@gmail.com>
2024-07-19 12:52:41 +0900

    캐시 이론 및 상황 테스트

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,39 +2,38 @@
 {
     internal class Program
     {
-        volatile static bool _stop = false; //C# = volatile 휘발성 데이터 선언 : 코드상 최적화를 하지 말아달라 라는 뜻
-        // 하지만 사용하지 말자 그냥 컴파일 릴리즈를 테스트하고 동작하는 것을 보기 위한 선언이다
+        /*만약 우리가 생각하는 수학적 이론이 맞다면 (y,x)와(x,y)가 동작하는 시간은 같아야 할 것이다.
+         하지만 실행해보면 시간이 거의 두 배의 차이가 난다. ((y,x)의 동작 시간이 더 빠르게 완료된다.)*/
 
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작");
-
-            while (_stop == false)
-            {
-                //누군가가 stop 신호를 해주기를 기다린다.
-            }
-
-
-            Console.WriteLine("쓰레드 종료");
-        }
-
-
-        // 핵심 기능
+        /*왜 그런가? -> 캐시의 동작 방식의 차이 때문이다. 캐시는 공간적 지역성이라고 방금 주문한 사람 근처에 있는 사람이
+         * 추가주문을 할 확률이 높다는 것의 개념이 있다.
+         * 어떤 변수에 접근을 하면 그 인적한 주소는 접근될 확률이 높을 것이다 
+         때문에 [][][][] [][][][] [][][][] [][][][] 이런 식의 배열이라면 인접한 변수가 더 빨리 실핼 되기에 시간의 차이가 발생한 것이다 */
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000);
+            {
+                long now = DateTime.Now.Ticks;
+                for(int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 : {end - now}");
+            }
 
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 : {end - now}");
+            }
         }
     }
 }
