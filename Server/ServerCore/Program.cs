6716ff7287a18d5a379f0c6b0516fd4b23116480
9909hjh <9909hjh@gmail.com>
2024-07-19 19:02:09 +0900

    이그노어 확인

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,29 +4,6 @@
 
     internal class Program
     {
-        int _anwser;
-        bool _complete;
-
-        /*read/write를 할 때 각각 Thread.MemoryBarrier()의 위치가 달라진다.*/
-
-        void A() // write를 할 때의 코드 // 선언 후 베리어
-        {
-            _anwser = 123;
-            Thread.MemoryBarrier(); // Barrier1
-            _complete = true;
-            Thread.MemoryBarrier(); // Barrier2 
-        }
-
-        void B() // read를 할때의 코드  // 사용하기 전 베리어
-        {
-            Thread.MemoryBarrier(); // Barrier3
-            if (_complete )
-            {
-                Thread.MemoryBarrier(); // Barrier4
-                Console.WriteLine(_anwser);
-            }
-        }
-
         static void Main(string[] args)
         {
             
