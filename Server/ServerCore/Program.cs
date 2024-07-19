93e05a666fab84dd2c9dcca018d04f66d796ddd5
9909hjh <9909hjh@gmail.com>
2024-07-19 13:28:32 +0900

    Thread.MemoryBarrier()함수로 코드 재배치 억제

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,13 +1,18 @@
 ﻿namespace ServerCore
 {
+    // 하드웨어 최적화를 방지하기 위한 메모리 베리어
+
+    //메모리 베리어
+    // A) 코드 재배치 억제
+    // B) 가시성
+
+    // 메모리 베리어 종류 
+    /*1) Full Memory Barrier (ASW WFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다.
+      2) Store Memory Barrier (ASW SFENCE) : Store만 막는다.
+      3) Load Memory Barrier (ASW LFENCE) : Load 막는다.*/
+
     internal class Program
     {
-        // 하드웨어 최적화를 방지하기 위한 메모리 베리어
-
-        //메모리 베리어
-        // A) 코드 재배치 억제
-        // B) 가시성
-
         static int x = 0;
         static int y = 0;
         static int r1 = 0;
@@ -15,12 +20,20 @@
 
         static void Thread_1()
         {
-            y = 1;
-            r1 = x;
+            y = 1; // Store y
+
+            Thread.MemoryBarrier();
+
+            // Load x
+            r1 = x; // 하드웨어에서 마음대로 먼저 실행이 되지 않게 함. MemoryBarrier()함수를 통해서
         }
         static void Thread_2()
         {
             x = 1;
+
+            Thread.MemoryBarrier();
+
+
             r2 = y;
         }
 
