9c8bbfe38b6b4e7c59f2c6965f14c2ad3cb51190
9909hjh <9909hjh@gmail.com>
2024-07-21 01:22:19 +0900

    Mutex 방식

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,36 +1,25 @@
 ﻿namespace ServerCore
 {
-    /* ManualResetEvent */
-    // 방 문같은 개념
-    class Lock
-    {
-        ManualResetEvent _available = new ManualResetEvent(true); // _available가 true면 누구나 들어올 수 있는 상태, false면 반대.
-        
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도.
-            _available.Reset(); // 문을 닫는다. // AutoReset과는 다르게 동작해서 Reset을 추가한다.
-                                // 나눠져서 값이 다르게 나온다.
-        }
-
-        public void Release()
-        {
-            _available?.Set(); // 문을 열어준다.
-        }
-    }
-
     internal class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+
+        // Mutex는 AutoReset과 같게 동작하지만 더 많은 정보를 갖고 있다.
+
+        // 갖는 정보 : int count, ThreadID ...
+        // 예를 들어 몇번이나 잠궜는지 카운팅을 하고 있다. 그래서 잠근 만큼 풀어줘야 최종적으로 풀리게 되어있다.
+        // 또한 ID를 갖고 있어 엉뚱한 곳에서 ReleaseMutex를 하는 것을 방지할 수 있다.
+
+        // 그래서 추가 비용이 많이 들어가고 느려서 어지간해서는 오토리셋을 사용한다.
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
             for(int  i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -38,9 +27,9 @@
         {
             for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
